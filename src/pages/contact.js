bcdb8dd38d09737544fbad4d512f716eaf11d3c0
carohadad <carolinahadad@gmail.com>
2025-07-11 17:23:52 -0300

    Added missing page

---
+++ src/pages/contact.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Layout from '@theme/Layout';
+import Link from '@docusaurus/Link';
+import global from '../css/custom.module.scss';
+import classNames from 'classnames';
+
+export default function Contact() {
+  return (
+    <Layout
+      title="Contat Shira Team"
+      description="TBD TBD"
+    >
+      <main className="main">
+
+        <div> TO BE CODED </div>
+      </main>
+    </Layout>
+  );
+}
