6f912fa0fbf7e26e486d80eeff31fd7cac2779ed
carohadad <carolinahadad@gmail.com>
2025-10-29 19:33:32 -0300

    removed so i can deploy

--- src/pages/about.js
+++ src/pages/about.js
@@ -59,7 +59,6 @@ export default function About() {
 
           <div className={classNames(styles.whyShiraButtons, global.row, global.center)}>
             <Link className={classNames(styles.button, global.buttonDark)} to="/privacy-policy">Privacy policy </Link>
-            <!-- <Link className={classNames(styles.button, global.buttonDark)} to="/assets/shira-final.pdf">Security Audit</Link> -->
           </div>
         </section>
 
