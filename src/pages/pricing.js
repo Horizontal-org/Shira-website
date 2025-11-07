49d3b86f2bee0603cbd272dc28e0cb7e040f6ec4
raphmim <27868414+raphmim@users.noreply.github.com>
2025-11-07 10:56:18 -0800

    Fixes and updates to pricing page

--- src/pages/pricing.js
+++ src/pages/pricing.js
@@ -29,7 +29,7 @@ export default function Pricing() {
             <h2>Starter</h2>
             <p className={styles.prize}>Free</p>
 
-            <p>Best to try things out.</p>
+            <p>Best to try things out</p>
 
 
             <Link className={global.buttonDark} to="/contact">Get started</Link>
@@ -40,17 +40,17 @@ export default function Pricing() {
 
             <h4>Admin management </h4>
             <ul>
-              <li>✔️ Cloud.</li>
-              <li>✔️ Admin dashboard.</li>
-              <li>✔️ 1 admin.</li>
+              <li>✔️ Cloud</li>
+              <li>✔️ Admin dashboard</li>
+              <li>✔️ 1 admin</li>
             <h4>Customization</h4>
-              <li>✔️ Fully customizable quiz questions.</li>
-              <li>✔️ Up to 3 custom quizzes.</li>
-              <li>✔️ Up to 5 questions per quiz.</li>
-              <li>✔️ Up to 1,500 quizzes taken per year.</li>
-              <li>✔️ 2 apps: Gmail, SMS.</li>
+              <li>✔️ Fully customizable quiz questions</li>
+              <li>✔️ Up to 3 custom quizzes</li>
+              <li>✔️ Up to 5 questions per quiz</li>
+              <li>✔️ Up to 1,500 quizzes taken per year</li>
+              <li>✔️ 2 apps: Gmail, SMS</li>
             <h4>Access control</h4>
-              <li>✔️Public quizzes: anyone can take your quiz through an easy-to-share URL.</li>
+              <li>✔️Public quizzes: anyone can take your quiz through an easy-to-share URL</li>
             </ul>
           </div>
 
@@ -58,7 +58,7 @@ export default function Pricing() {
            <div className={classNames(global.backgroundLight,styles.plan)}>
             <h2>Pro</h2>
             <p className={styles.prize}>Flat fee</p>
-            <p>Best for organizations looking to level-up.</p>
+            <p>Best for organizations looking to level-up</p>
 
 
             <Link className={global.buttonDark} to="/contact">Get started</Link>
@@ -69,25 +69,25 @@ export default function Pricing() {
 
             <h4>Admin management </h4>
             <ul>
-              <li>✔️ Cloud.</li>
-              <li>✔️ Admin dashboard.</li>
-              <li>✔️ Unlimited admins.</li>
+              <li>✔️ Cloud</li>
+              <li>✔️ Admin dashboard</li>
+              <li>✔️ Unlimited admins [coming soon]</li>
             <h4>Customization</h4>
-              <li>✔️ Fully customizable quiz questions.</li>
-              <li>✔️ Unlimited custom quizzes.</li>
-              <li>✔️ Unlimited quiz questions.</li>
-              <li>✔️ Up to 1,500 quizzes taken per year*.</li>
-              <li>✔️ 5 app: Gmail, SMS, WhatsApp, Messenger, Dating app.</li>
+              <li>✔️ Fully customizable quiz questions</li>
+              <li>✔️ Unlimited custom quizzes</li>
+              <li>✔️ Unlimited quiz questions</li>
+              <li>✔️ Up to 1,500 quizzes taken per year*</li>
+              <li>✔️ 6 apps: Gmail, Outlook, SMS, WhatsApp, Messenger, Dating app</li>
             <h4>Access control</h4>
-              <li>✔️Public quizzes: anyone can take your quiz through an easy-to-share URL.</li>
-              <li>✔️Private quizzes: only your users can take your quizzes.</li>
+              <li>✔️Public quizzes: anyone can take your quiz through an easy-to-share URL</li>
+              <li>✔️Private quizzes: only your users can take your quizzes</li>
             <h4>Compliance</h4>
-              <li>✔️Track user progress.</li>
-              <li>✔️Analysis and compliance tools.</li>
+              <li>✔️Track user progress</li>
+              <li>✔️Analysis and compliance tools [coming soon]</li>
             </ul>
 
             <p className={styles.note}>
-              * Additional quiz packages available if needed.
+              * Additional quiz packages available if needed
             </p>
           </div>
 
@@ -108,25 +108,25 @@ export default function Pricing() {
 
             <h4>Admin management </h4>
             <ul>
-              <li>✔️ Cloud or on-premise.</li>
-              <li>✔️ Admin dashboard.</li>
-              <li>✔️ Unlimited admins.</li>
+              <li>✔️ Cloud or on-premise</li>
+              <li>✔️ Admin dashboard</li>
+              <li>✔️ Unlimited admins</li>
             <h4>Customization</h4>
-              <li>✔️ Fully customizable quiz questions.</li>
-              <li>✔️ Unlimited custom quizzes.</li>
-              <li>✔️ Unlimited quiz questions.</li>
+              <li>✔️ Fully customizable quiz questions</li>
+              <li>✔️ Unlimited custom quizzes</li>
+              <li>✔️ Unlimited quiz questions</li>
               <li>✔️ Custom number of quizzes taken</li>
-              <li>✔️ 5 app: Gmail, SMS, WhatsApp, Messenger, Dating app.</li>
+              <li>✔️ 6 apps: Gmail, Outlook, SMS, WhatsApp, Messenger, Dating app</li>
             <h4>Access control</h4>
-              <li>✔️Public quizzes: anyone can take your quiz through an easy-to-share URL.</li>
-              <li>✔️Private quizzes: only your users can take your quizzes.</li>
+              <li>✔️Public quizzes: anyone can take your quiz through an easy-to-share URL</li>
+              <li>✔️Private quizzes: only your users can take your quizzes</li>
             <h4>Compliance</h4>
-              <li>✔️Track user progress.</li>
-              <li>✔️Analysis and compliance tools.</li>
+              <li>✔️Track user progress</li>
+              <li>✔️Analysis and compliance tools [coming soon]</li>
 
             <h4>Custom support</h4>
-              <li>✔️Tailor-made user training.</li>
-              <li>✔️Priority support.</li>
+              <li>✔️Tailor-made user training</li>
+              <li>✔️Priority support</li>
             </ul>
 
 
