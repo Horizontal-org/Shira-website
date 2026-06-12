42a1b14e1fa850f78a78a808d2d001360772487b
carohadad <carolinahadad@gmail.com>
2026-06-12 16:50:08 -0300

    used existing styles and variables so that the  pricing component works well in dark mode

--- src/theme/Root.tsx
+++ src/theme/Root.tsx
@@ -2,9 +2,31 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { defaultTheme } from '@horizontal-org/shira-ui';
 
+const theme = {
+  ...defaultTheme,
+  colors: {
+    ...defaultTheme.colors,
+    dark: {
+      black:      'var(--ifm-color-primary-darkest)',
+      lightGrey:  'var(--ifm-color-primary-lighter)',
+      darkGrey:   'var(--ifm-color-primary-darker)',
+      mediumGrey: 'var(--ifm-color-primary-darker)',
+      overlay:    defaultTheme.colors.dark.overlay,
+    },
+    light: {
+      white:     'var(--ifm-color-primary-lightest)',
+      paleGrey:  'var(--ifm-color-primary-lighter)',
+      paleGreen: 'var(--ifm-color-primary-light)',
+      paleRed:   defaultTheme.colors.light.paleRed,
+    },
+    green7: 'var(--ifm-color-primary)',
+    green6: 'var(--ifm-color-primary)',
+  },
+};
+
 export default function Root({ children }: { children: React.ReactNode }) {
   return (
-    <ThemeProvider theme={defaultTheme}>
+    <ThemeProvider theme={theme}>
       {children}
     </ThemeProvider>
   );
